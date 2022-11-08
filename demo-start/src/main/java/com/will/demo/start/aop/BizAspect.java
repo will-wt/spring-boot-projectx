c94a53274854eb7ba34d2ae0260ee6437c31b6aa
Will.WT <willwt@qq.com>
2022-11-09 00:19:42 +0800

    add sql aspect

--- demo-start/src/main/java/com/will/demo/start/aop/BizAspect.java
+++ demo-start/src/main/java/com/will/demo/start/aop/BizAspect.java
@@ -7,33 +7,36 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Will.WT
  * @date 2022/11/07 23:49
  */
+@Component
 @Aspect
 public class BizAspect {
 
     private static final Logger sqlLog = LoggerFactory.getLogger("sqlLog");
 
-    @Around(value = "execution(* com.will.demo.dal.mapper.*.*.*(..))")
+    @Around("execution(* com.will.demo.dal.mapper..*.*(..))")
     public Object dalAround(ProceedingJoinPoint joinPoint){
         long startTime = System.currentTimeMillis();
         Object[] args = joinPoint.getArgs();
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         String className = signature.getDeclaringType().getSimpleName();
         String methodName = signature.getMethod().getName();
+        boolean success = true;
 
         try {
             return joinPoint.proceed(args);
         } catch (Throwable e) {
-            e.printStackTrace();
+            success = false;
             throw new RuntimeException(e);
         } finally {
             long useTime = System.currentTimeMillis() - startTime;
-            sqlLog.info("class={};method={};useTime={};args={};result={}",
-                    className, methodName, useTime, JacksonHelper.toJson(args), null);
+            sqlLog.info("class={};method={};useTime={}ms;success={};args={};result={}",
+                    className, methodName, useTime, success, JacksonHelper.toJson(args), null);
         }
     }
 
